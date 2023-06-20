4988a2fb94e8278bf99dd3d29a96c58946d2325b
Aadi Poddar <aadipoddarmail@gmail.com>
2023-06-20 21:23:55 +0530

    Added events to ImGui layer

--- Hazel/src/Hazel/ImGui/ImGuiLayer.h
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Hazel\Layer.h"
 
+#include "Hazel\Events\ApplicationEvent.h"
+#include "Hazel\Events\KeyEvent.h"
+#include "Hazel\Events\MouseEvent.h"
+
 namespace Hazel {
 
 	class HAZEL_API ImGuiLayer : public Layer
@@ -14,6 +18,16 @@ namespace Hazel {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& event);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& event);
+		bool OnMouseMovedEvent(MouseMovedEvent& event);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& event);
+		bool OnKeyPressedEvent(KeyPressedEvent& event);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& event);
+		bool OnKeyTypedEvent(KeyTypedEvent& event);
+		bool OnWindowResizeEvent(WindowResizeEvent& event);
 
 	private:
 		float m_Time = 0.0f;
